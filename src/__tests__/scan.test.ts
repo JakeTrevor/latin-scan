7347cb800002cf8b19071c9abf4a46fdf6787e86
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2021-09-05 12:12:46 +0100

    none of the build files should be tracked by git, bit confused why they're here to begin with...

--- src/__tests__/scan.test.ts
+++ src/__tests__/scan.test.ts
@@ -1,220 +1,220 @@
-import type {
-  quantity,
-  scannedLineType,
-  syllableMap,
-} from "src/SCAN/scanTypes";
-import { PresetOptions } from "../SCAN/utils";
-import {
-  scanParagraph,
-  scanLine,
-  removePunctuation,
-  preScan,
-  postScan,
-  analyseHex,
-  arrToQuantity,
-  marryUp,
-  insertPunctuation,
-} from "../SCAN/scan";
+// import type {
+//   quantity,
+//   scannedLineType,
+//   syllableMap,
+// } from "src/SCAN/scanTypes";
+// import { PresetOptions } from "../SCAN/utils";
+// import {
+//   scanParagraph,
+//   scanLine,
+//   removePunctuation,
+//   preScan,
+//   postScan,
+//   analyseHex,
+//   arrToQuantity,
+//   marryUp,
+//   insertPunctuation,
+// } from "../SCAN/scan";
 
-describe("scan algorithm functionality testing", () => {
-  //test if each of the functions in scan.ts *works* in some case
-  //none of these are proper stress tests, which would look at edge cases.
+// describe("scan algorithm functionality testing", () => {
+//   //test if each of the functions in scan.ts *works* in some case
+//   //none of these are proper stress tests, which would look at edge cases.
 
-  //some vairbles used throughout the testing:
-  let AeneidLn1 = "Arma virumque cano Troiae qui primus ab oris";
-  let AeneidNatQuants: syllableMap = {
-    //short for Aeneidln1 natural quantities
-    0: "long",
-    3: "undefined",
-    6: "undefined",
-    8: "long",
-    12: "undefined",
-    15: "undefined",
-    17: "long",
-    21: "long",
-    23: "long",
-    28: "long",
-    32: "undefined",
-    34: "undefined",
-    37: "undefined",
-    40: "undefined",
-    42: "undefined",
-  };
-  let Aeneid1Scan: syllableMap = {
-    "0": "long",
-    "12": "short",
-    "15": "short",
-    "17": "long",
-    "21": "long",
-    "23": "long",
-    "28": "long",
-    "3": "short",
-    "32": "long",
-    "34": "short",
-    "37": "short",
-    "40": "long",
-    "42": "undefined",
-    "6": "short",
-    "8": "long",
-  };
-  let AeneidMarked = "Ārmă vĭ|rūmquĕ că|nō Trōi|āe quī| prīmŭs ăb| ōris";
-  let AeneidLn1Done: scannedLineType = {
-    line: AeneidLn1,
-    output: [
-      {
-        raw: "Ārma virūmque canō Trōiāe quī primus ab oris",
-        full: [AeneidMarked],
-      },
-    ],
-    errors: [],
-  };
+//   //some vairbles used throughout the testing:
+//   let AeneidLn1 = "Arma virumque cano Troiae qui primus ab oris";
+//   let AeneidNatQuants: syllableMap = {
+//     //short for Aeneidln1 natural quantities
+//     0: "long",
+//     3: "undefined",
+//     6: "undefined",
+//     8: "long",
+//     12: "undefined",
+//     15: "undefined",
+//     17: "long",
+//     21: "long",
+//     23: "long",
+//     28: "long",
+//     32: "undefined",
+//     34: "undefined",
+//     37: "undefined",
+//     40: "undefined",
+//     42: "undefined",
+//   };
+//   let Aeneid1Scan: syllableMap = {
+//     "0": "long",
+//     "12": "short",
+//     "15": "short",
+//     "17": "long",
+//     "21": "long",
+//     "23": "long",
+//     "28": "long",
+//     "3": "short",
+//     "32": "long",
+//     "34": "short",
+//     "37": "short",
+//     "40": "long",
+//     "42": "undefined",
+//     "6": "short",
+//     "8": "long",
+//   };
+//   let AeneidMarked = "Ārmă vĭ|rūmquĕ că|nō Trōi|āe quī| prīmŭs ăb| ōris";
+//   let AeneidLn1Done: scannedLineType = {
+//     line: AeneidLn1,
+//     output: [
+//       {
+//         raw: "Ārma virūmque canō Trōiāe quī primus ab oris",
+//         full: [AeneidMarked],
+//       },
+//     ],
+//     errors: [],
+//   };
 
-  test.todo("scan paragraph");
+//   test.todo("scan paragraph");
 
-  test("scan line", () => {
-    expect(scanLine(AeneidLn1, PresetOptions.meter)).toEqual(AeneidLn1Done);
-  });
+//   test("scan line", () => {
+//     expect(scanLine(AeneidLn1, PresetOptions.meter)).toEqual(AeneidLn1Done);
+//   });
 
-  test("undress", () => {
-    let input =
-      "this: is; a, line? to be undressed! it has a bunch of punctuation.";
-    let output = [
-      { 4: ":", 8: ";", 11: ",", 17: "?", 34: "!", 65: "." },
-      "this is a line to be undressed it has a bunch of punctuation",
-    ];
-    expect(removePunctuation(input)).toEqual(output);
-  });
+//   test("undress", () => {
+//     let input =
+//       "this: is; a, line? to be undressed! it has a bunch of punctuation.";
+//     let output = [
+//       { 4: ":", 8: ";", 11: ",", 17: "?", 34: "!", 65: "." },
+//       "this is a line to be undressed it has a bunch of punctuation",
+//     ];
+//     expect(removePunctuation(input)).toEqual(output);
+//   });
 
-  test("pre-scan", () => {
-    //let input2 = "Italiam fato profugus Laviniaque venit";
-    expect(preScan(AeneidLn1)).toEqual([AeneidNatQuants]);
-  });
+//   test("pre-scan", () => {
+//     //let input2 = "Italiam fato profugus Laviniaque venit";
+//     expect(preScan(AeneidLn1)).toEqual([AeneidNatQuants]);
+//   });
 
-  test("post-scan", () => {
-    let breaks = [7, 16, 22, 29, 38];
-    expect(postScan(AeneidLn1, {}, Aeneid1Scan, breaks)).toEqual(AeneidMarked);
-  });
+//   test("post-scan", () => {
+//     let breaks = [7, 16, 22, 29, 38];
+//     expect(postScan(AeneidLn1, {}, Aeneid1Scan, breaks)).toEqual(AeneidMarked);
+//   });
 
-  test("hex scan", () => {
-    let output: [syllableMap, number[]][] = [];
-    output.push([Aeneid1Scan, [7, 16, 22, 29, 38]]);
+//   test("hex scan", () => {
+//     let output: [syllableMap, number[]][] = [];
+//     output.push([Aeneid1Scan, [7, 16, 22, 29, 38]]);
 
-    expect(analyseHex(AeneidNatQuants)).toEqual([
-      [Aeneid1Scan, [7, 16, 22, 29, 38]],
-    ]);
-  });
+//     expect(analyseHex(AeneidNatQuants)).toEqual([
+//       [Aeneid1Scan, [7, 16, 22, 29, 38]],
+//     ]);
+//   });
 
-  test("array to quantity", () => {
-    let input = [
-      [0, 0, 0, 0],
-      [1, 1, 1, 1],
-      [0, 1, 1, 0],
-    ]; // three random test cases
-    let output: quantity[][] = [
-      [
-        "long",
-        "long",
-        "break",
-        "long",
-        "long",
-        "break",
-        "long",
-        "long",
-        "break",
-        "long",
-        "long",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "undefined",
-      ],
-      [
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "undefined",
-      ],
-      [
-        "long",
-        "long",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "long",
-        "break",
-        "long",
-        "short",
-        "short",
-        "break",
-        "long",
-        "undefined",
-      ],
-    ];
-    expect(arrToQuantity(input, "Hexameter")).toEqual(output);
-  });
+//   test("array to quantity", () => {
+//     let input = [
+//       [0, 0, 0, 0],
+//       [1, 1, 1, 1],
+//       [0, 1, 1, 0],
+//     ]; // three random test cases
+//     let output: quantity[][] = [
+//       [
+//         "long",
+//         "long",
+//         "break",
+//         "long",
+//         "long",
+//         "break",
+//         "long",
+//         "long",
+//         "break",
+//         "long",
+//         "long",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "undefined",
+//       ],
+//       [
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "undefined",
+//       ],
+//       [
+//         "long",
+//         "long",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "long",
+//         "break",
+//         "long",
+//         "short",
+//         "short",
+//         "break",
+//         "long",
+//         "undefined",
+//       ],
+//     ];
+//     expect(arrToQuantity(input, "Hexameter")).toEqual(output);
+//   });
 
-  test("marry up", () => {
-    let quantities: quantity[] = [
-      "long",
-      "short",
-      "break",
-      "long",
-      "short",
-      "long",
-      "long",
-    ];
-    let positions = [11, 32, 34, 40, 50, 51];
-    let ans = [
-      {
-        11: "long",
-        32: "short",
-        34: "long",
-        40: "short",
-        50: "long",
-        51: "long",
-      },
-      [33],
-    ];
-    expect(marryUp(quantities, positions)).toEqual(ans);
-  });
+//   test("marry up", () => {
+//     let quantities: quantity[] = [
+//       "long",
+//       "short",
+//       "break",
+//       "long",
+//       "short",
+//       "long",
+//       "long",
+//     ];
+//     let positions = [11, 32, 34, 40, 50, 51];
+//     let ans = [
+//       {
+//         11: "long",
+//         32: "short",
+//         34: "long",
+//         40: "short",
+//         50: "long",
+//         51: "long",
+//       },
+//       [33],
+//     ];
+//     expect(marryUp(quantities, positions)).toEqual(ans);
+//   });
 
-  test("insert punctuation", () => {
-    let markup = { 4: ":", 8: ";", 11: ",", 17: "?", 34: "!", 65: "." };
-    let line = Array.from(
-      "this is a line to be undressed it has a bunch of punctuation"
-    );
-    let ans = Array.from(
-      "this: is; a, line? to be undressed! it has a bunch of punctuation."
-    );
+//   test("insert punctuation", () => {
+//     let markup = { 4: ":", 8: ";", 11: ",", 17: "?", 34: "!", 65: "." };
+//     let line = Array.from(
+//       "this is a line to be undressed it has a bunch of punctuation"
+//     );
+//     let ans = Array.from(
+//       "this: is; a, line? to be undressed! it has a bunch of punctuation."
+//     );
 
-    expect(insertPunctuation(line, markup)).toEqual(ans);
-  });
-});
+//     expect(insertPunctuation(line, markup)).toEqual(ans);
+//   });
+// });
--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,60 +1,60 @@
-import expressions, {
-  findAllMatches,
-  matchAndMap,
-  nBitCombos,
-  sum,
-} from "../SCAN/utils";
+// import expressions, {
+//   findAllMatches,
+//   matchAndMap,
+//   nBitCombos,
+//   sum,
+// } from "../SCAN/utils";
 
-describe("utilities tests", () => {
-  test("array summer", () => {
-    expect(sum([1, 1, 1, 1])).toEqual(4);
-  });
+// describe("utilities tests", () => {
+//   test("array summer", () => {
+//     expect(sum([1, 1, 1, 1])).toEqual(4);
+//   });
 
-  //? testing the combinatorial generator
+//   //? testing the combinatorial generator
 
-  let ans2 = ["00", "01", "10", "11"];
-  let ans3 = "000 001 010 011 100 101 110 111".split(" ");
-  test("nBitCombos with toString", () => {
-    expect(nBitCombos(2)).toEqual(ans2);
-    expect(nBitCombos(3)).toEqual(ans3);
-  });
+//   let ans2 = ["00", "01", "10", "11"];
+//   let ans3 = "000 001 010 011 100 101 110 111".split(" ");
+//   test("nBitCombos with toString", () => {
+//     expect(nBitCombos(2)).toEqual(ans2);
+//     expect(nBitCombos(3)).toEqual(ans3);
+//   });
 
-  //? testing regex functions
+//   //? testing regex functions
 
-  let string = "this is a string, can you see that it is a string?";
-  let regex = /is/g;
-  let matchPositions = [
-    2, 5, 8, 13, 19, 22, 23, 24, 27, 28, 32, 35, 38, 41, 46,
-  ];
+//   let string = "this is a string, can you see that it is a string?";
+//   let regex = /is/g;
+//   let matchPositions = [
+//     2, 5, 8, 13, 19, 22, 23, 24, 27, 28, 32, 35, 38, 41, 46,
+//   ];
 
-  test("RegEx finder", () => {
-    expect(findAllMatches(string, regex)).toEqual([2, 5, 38]);
-    expect(findAllMatches(string, expressions.vowels)).toEqual(matchPositions);
-  });
+//   test("RegEx finder", () => {
+//     expect(findAllMatches(string, regex)).toEqual([2, 5, 38]);
+//     expect(findAllMatches(string, expressions.vowels)).toEqual(matchPositions);
+//   });
 
-  regex = /is/g;
-  test("RegEx match", () => {
-    expect(matchAndMap(string, regex)).toEqual({
-      "2": "is",
-      "5": "is",
-      "38": "is",
-    });
-    expect(matchAndMap(string, expressions.vowels)).toEqual({
-      "2": "i",
-      "5": "i",
-      "8": "a",
-      "13": "i",
-      "19": "a",
-      "22": "y",
-      "23": "o",
-      "24": "u",
-      "27": "e",
-      "28": "e",
-      "32": "a",
-      "35": "i",
-      "38": "i",
-      "41": "a",
-      "46": "i",
-    });
-  });
-});
+//   regex = /is/g;
+//   test("RegEx match", () => {
+//     expect(matchAndMap(string, regex)).toEqual({
+//       "2": "is",
+//       "5": "is",
+//       "38": "is",
+//     });
+//     expect(matchAndMap(string, expressions.vowels)).toEqual({
+//       "2": "i",
+//       "5": "i",
+//       "8": "a",
+//       "13": "i",
+//       "19": "a",
+//       "22": "y",
+//       "23": "o",
+//       "24": "u",
+//       "27": "e",
+//       "28": "e",
+//       "32": "a",
+//       "35": "i",
+//       "38": "i",
+//       "41": "a",
+//       "46": "i",
+//     });
+//   });
+// });
