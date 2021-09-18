2d5705353c0bd5bd3386b6004741788879942cad
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2021-09-18 17:47:26 +0100

    updated ICONS file and added new SVGs

--- src/components/ICONS/ICONS.tsx
+++ src/components/ICONS/ICONS.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import type { ReactElement } from "react";
 import DownArrow from "./DownArrow.svg";
 import Hexagon from "./Hexagon.svg";
+import HexPlus from "./HexPlus.svg";
+import HexWarn from "./HexWarn.svg";
 import Pentagon from "./Pentagon.svg";
+import PentPlus from "./PentPlus.svg";
+import PentWarn from "./PentWarn.svg";
 import Warning from "./Warning.svg";
 import Tick from "./Tick.svg";
 import Input from "./Input.svg";
@@ -9,21 +14,52 @@ import Bar from "./Bar.svg";
 import Camera from "./Camera.svg";
 import Keyboard from "./Keyboard.svg";
 import TextFileInput from "./TextFileInput.svg";
+import type {
+  meter,
+  scanNature,
+  scanStatus,
+} from "latin-scanner/build/src/types";
+import Tooltip from "../Tooltip";
 
 //put all the icons imports in one place, keeps things clean.
-const ICONS: Record<string, JSX.Element> = {
+export let misc: Record<string, ReactElement> = {
   DownArrow: <DownArrow className="icon" />,
-  HexameterOK: <Hexagon className="icon" />,
-  "Hexameter+": <Hexagon className="icon" />,
-  PentameterOK: <Pentagon className="icon" />,
-  "Pentameter+": <Pentagon className="icon" />,
-  Warning: <Warning className="icon warning" />,
-  Tick: <Tick className="icon" />,
-  Input: <Input className="icon" />,
-  Bar: <Bar className="icon" />,
   Camera: <Camera className="icon" />,
   Keyboard: <Keyboard className="icon" />,
   TextFileInput: <TextFileInput className="icon" />,
+  Warning: <Warning className="icon" />,
 };
 
-export default ICONS;
+export let scanNatureIcons: Record<scanNature, ReactElement> = {
+  "Full Scan": (
+    <Tooltip tooltip="This is a full Scan">
+      <Tick className="icon" />
+    </Tooltip>
+  ),
+  Quantities: (
+    <Tooltip tooltip="This line contains only certain quantities.">
+      <Bar className="icon" />
+    </Tooltip>
+  ),
+  Input: (
+    <Tooltip tooltip="This line is your input.">
+      <Input className="icon" />
+    </Tooltip>
+  ),
+};
+
+export let meterAndStatusIcons: Record<
+  meter,
+  Record<scanStatus, ReactElement>
+> = {
+  Hexameter: {
+    OK: <Hexagon className="icon" />,
+    Plus: <HexPlus className="icon" />,
+    Warning: <HexWarn className="icon" />,
+  },
+  Pentameter: {
+    OK: <Pentagon className="icon" />,
+    Plus: <PentPlus className="icon" />,
+    Warning: <PentWarn className="icon" />,
+  },
+};
